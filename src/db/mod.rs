ac9c800b7087379ec93fe8f6f80c19c018e1b4cc
AlterionX <ben.xu.cs@gmail.com>
2025-12-17 00:58:54 -0800

    checkpoint for monthly goal

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,8 +2,10 @@ mod navy;
 mod legion;
 mod industry;
 mod event;
+mod monthly_goal;
 
 pub use navy::*;
 pub use legion::*;
 pub use industry::*;
 pub use event::*;
+pub use monthly_goal::*;
---
+++ src/db/monthly_goal.rs
@@ -0,0 +1,53 @@
+use bigdecimal::BigDecimal;
+use chrono::{DateTime, Utc};
+use diesel::{ExpressionMethods, OptionalExtension, QueryDsl, prelude::{Identifiable, Insertable, Queryable}};
+use diesel_async::RunQueryDsl;
+use crate::schema;
+
+use azel::db::{Connector, DbResult};
+
+#[derive(Debug, Clone)]
+#[derive(Insertable)]
+#[diesel(table_name = schema::monthly_goals)]
+pub struct NewMonthlyGoal {
+    pub updater: BigDecimal,
+    pub tag: String,
+    pub header: String,
+    pub body: String,
+}
+
+#[derive(Debug, Clone)]
+#[derive(Insertable, Queryable, Identifiable)]
+#[diesel(table_name = schema::monthly_goals)]
+pub struct MonthlyGoal {
+    pub id: BigDecimal,
+    pub created: DateTime<Utc>,
+    pub updater: BigDecimal,
+    pub tag: String,
+    pub header: String,
+    pub body: String,
+}
+
+impl MonthlyGoal {
+    pub async fn create(connection_maker: &impl Connector, new: NewMonthlyGoal) -> DbResult<()> {
+        let mut conn = connection_maker.async_connect().await?;
+
+        diesel::insert_into(schema::monthly_goals::table)
+            .values(new)
+            .execute(&mut conn)
+            .await?;
+
+        Ok(())
+    }
+
+    pub async fn load_for(connection_maker: &impl Connector, tag: &str) -> DbResult<Option<Self>> {
+        let mut conn = connection_maker.async_connect().await?;
+        Ok(schema::monthly_goals::table
+            .filter(schema::monthly_goals::tag.eq(tag))
+            .order(schema::monthly_goals::created.desc())
+            .limit(1)
+            .get_result(&mut conn)
+            .await
+            .optional()?)
+    }
+}
