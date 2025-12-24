3a6e47700c830d317c497de0e05052bb3105e334
AlterionX <ben.xu.cs@gmail.com>
2025-12-23 18:33:38 -0800

    Patch monthly goal for new values

--- src/db/monthly_goal.rs
+++ src/db/monthly_goal.rs
@@ -1,6 +1,6 @@
 use bigdecimal::BigDecimal;
 use chrono::{DateTime, Utc};
-use diesel::{ExpressionMethods, OptionalExtension, QueryDsl, prelude::{Identifiable, Insertable, Queryable}};
+use diesel::{ExpressionMethods, OptionalExtension, QueryDsl, prelude::{AsChangeset, Identifiable, Insertable, Queryable}};
 use diesel_async::RunQueryDsl;
 use crate::schema;
 
@@ -12,8 +12,19 @@ use azel::db::{Connector, DbResult};
 pub struct NewMonthlyGoal<'a> {
     pub updater: BigDecimal,
     pub tag: &'a str,
-    pub header: &'a str,
-    pub body: &'a str,
+    pub header: Option<&'a str>,
+    pub body: Option<&'a str>,
+    pub progress: Option<i16>,
+    pub shortname: &'a str,
+}
+
+#[derive(Debug, Clone)]
+#[derive(Queryable, AsChangeset)]
+#[diesel(table_name = schema::monthly_goals)]
+pub struct MonthlyGoalUpdate<'a> {
+    pub header: Option<&'a str>,
+    pub body: Option<&'a str>,
+    pub progress: Option<i16>,
 }
 
 #[derive(Debug, Clone)]
@@ -26,24 +37,34 @@ pub struct MonthlyGoal {
     pub tag: String,
     pub header: String,
     pub body: String,
+    pub progress: i16,
+    pub shortname: String,
+    pub active: bool,
 }
 
 impl MonthlyGoal {
-    pub async fn create(connection_maker: &impl Connector, new: NewMonthlyGoal<'_>) -> DbResult<()> {
+    pub async fn upsert(connection_maker: &impl Connector, new: NewMonthlyGoal<'_>) -> DbResult<()> {
         let mut conn = connection_maker.async_connect().await?;
 
         diesel::insert_into(schema::monthly_goals::table)
-            .values(new)
+            .values(new.clone())
+            .on_conflict(schema::monthly_goals::shortname)
+            .do_update()
+            .set(&MonthlyGoalUpdate {
+                header: new.header,
+                body: new.body,
+                progress: new.progress,
+            })
             .execute(&mut conn)
             .await?;
 
         Ok(())
     }
 
-    pub async fn load_for(connection_maker: &impl Connector, tag: &str) -> DbResult<Option<Self>> {
+    pub async fn load_for(connection_maker: &impl Connector, shortname: &str) -> DbResult<Option<Self>> {
         let mut conn = connection_maker.async_connect().await?;
         Ok(schema::monthly_goals::table
-            .filter(schema::monthly_goals::tag.eq(tag))
+            .filter(schema::monthly_goals::shortname.eq(shortname))
             .order(schema::monthly_goals::created.desc())
             .limit(1)
             .get_result(&mut conn)
