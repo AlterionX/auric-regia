2940c7bd19ace689c7faf764d912174f37377bb4
AlterionX <ben.xu.cs@gmail.com>
2026-05-10 18:54:43 -0700

    migrate branch tag for goals

---
+++ migrations/2026-05-11-012828_goal_branch_rename_for_lumi/down.sql
@@ -0,0 +1,13 @@
+UPDATE monthly_goals
+SET
+    tag = 'legion'
+WHERE
+    guild_id = 1455119921334845452
+    AND tag = 'ground';
+
+UPDATE monthly_goals
+SET
+    tag = 'industry'
+WHERE
+    guild_id = 1455119921334845452
+    AND tag = 'industrial';
---
+++ migrations/2026-05-11-012828_goal_branch_rename_for_lumi/up.sql
@@ -0,0 +1,13 @@
+UPDATE monthly_goals
+SET
+    tag = 'ground'
+WHERE
+    guild_id = 1455119921334845452
+    AND tag = 'legion';
+
+UPDATE monthly_goals
+SET
+    tag = 'industrial'
+WHERE
+    guild_id = 1455119921334845452
+    AND tag = 'industry';
